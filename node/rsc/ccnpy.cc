a06260f1209ca0355c7e639480cf67d60eb3080e
Tuna Fish <tunafish2k@163.com>
2023-01-20 20:40:39 +0800

    idk what

--- node/rsc/ccnpy.cc
+++ node/rsc/ccnpy.cc
@@ -1,15 +1,26 @@
 #include <Python.h>
 #include <napi.h>
+#include<string>
 
 using namespace Napi;
 
-Object helloTo (const CallbackInfo& info) {
+Value helloTo (const CallbackInfo& info) {
     Env env = info.Env();
     Object helloResult = Object::New(env);
     helloResult.Set(String::New(env,"count"),Number::New(env,info.Length));
     Array helloDetails = Array:New(env);
+    string greeting;
     for (int i = 0;i < info.Length;i++) {
-        helloDetails.Set(Number::New)
+        greeting="Hello, " + info[i].As<String>();
+        helloDetails.Set(Number::New(env,i),String::New(env,greeting));
     }
     helloResult.Set(String::New(env,"details"),helloDetails);
+    return helloResult;
 }
+
+Object Init(Env env, Object exports){
+    exports.Set(String::New(env,"HelloTo"),Function::New(env,helloTo));
+    return exports;
+}
+
+NODE_API_MODULE(addon,Init);
